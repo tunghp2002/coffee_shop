38672aabc03911102141d5b1498d4e3651fcded8
tunghp2002 <atsimet@gmail.com>
2024-06-09 17:24:00 +0700

    fixed clerk route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
   const svix_signature = headerPayload.get('svix-signature');
 
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
+    return new Response('Error occurred -- no svix headers', {
       status: 400,
     });
   }
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
+    return new Response('Error occurred', {
       status: 400,
     });
   }
@@ -55,9 +55,9 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      username: username || '', 
+      firstName: first_name || '', 
+      lastName: last_name || '', 
       photo: image_url,
     };
 
@@ -78,9 +78,9 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
+      firstName: first_name || '',
+      lastName: last_name || '',
+      username: username || '',
       photo: image_url,
     };
 
@@ -92,7 +92,7 @@ export async function POST(req: Request) {
   if (eventType === 'user.deleted') {
     const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id!);
+    const deletedUser = await deleteUser(id);
 
     return NextResponse.json({ message: 'OK', user: deletedUser });
   }
